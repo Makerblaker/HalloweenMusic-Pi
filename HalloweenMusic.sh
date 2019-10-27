d51f4eff112360ace4db7e7560b6b4a4c8e7537b
makerblaker <blake@blakemiller.ca>
2019-10-27 11:12:06 -0400

    Init Commit

---
+++ HalloweenMusic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Press [CTRL+C] to stop.."
+
+while :
+do
+	omxplayer Halloween\ Sound\ Track\ 01.mp3
+	sleep 1
+	omxplayer Black\ Sabbath\ -\ Bells\ Extended.mp3
+	sleep 1
+done
